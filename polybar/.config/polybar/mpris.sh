ff31c2f317613cb22ddfb90a6f476ce8b4922b92
Gabe Gordon <gabegordon1234@gmail.com>
2017-05-22 22:46:27 +0000

    Stow dotfiles

---
+++ polybar/.config/polybar/mpris.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Specifying the icon(s) in the script
+# This allows us to change its appearance conditionally
+icon=""
+
+player_status=$(playerctl status 2> /dev/null)
+if [[ $? -eq 0 ]]; then
+    metadata="$(playerctl metadata artist) - $(playerctl metadata title)"
+fi
+
+# Foreground color formatting tags are optional
+if [[ $player_status = "Playing" ]]; then
+    echo "%{F#232c34}$icon $metadata"       # Orange when playing
+else
+    echo ""                 # Greyed out icon when 
+stopped
+fi
