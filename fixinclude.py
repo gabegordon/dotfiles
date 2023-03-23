0a52d54c703e4d0e28141eb51c0f4ad71cd4d22c
Gabe Gordon <ggordon@persistentsystems.com>
2023-03-23 13:07:12 -0400

    only add cross target for non kernel compiles

--- fixinclude.py
+++ fixinclude.py
@@ -8,14 +8,15 @@ data = json.load(f)
 for i in data:
     if (i['arguments'][0] == "/usr/local/bin/arm-eabi-gcc"):
         i['arguments'][0] = "/home/ggordon/repo/vendor/persistent/arm-eabi/bin/arm-eabi-gcc"
-    if (i['arguments'][0] == "/usr/local/bin/arm-linux-androideabi-clang"):
-        i['arguments'][0] = "/home/ggordon/repo/vendor/persistent/standalone-toolchain/bin/arm-linux-androideabi-clang"
-    if (i['arguments'][0] == "/usr/local/bin/arm-linux-androideabi-clang++"):
-        i['arguments'][0] = "/home/ggordon/repo/vendor/persistent/standalone-toolchain/bin/arm-linux-androideabi-clang++"
-    i['arguments'].insert(1, "--target=armv7a-linux-androideabi28")
-    i['arguments'].insert(1, "--sysroot=/home/ggordon/repo/vendor/persistent/standalone-toolchain/sysroot")
-    i['arguments'].insert(2, "-I")
-    i['arguments'].insert(3, "/home/ggordon/repo/vendor/persistent/standalone-toolchain/sysroot/usr/include")
+    else:
+        if (i['arguments'][0] == "/usr/local/bin/arm-linux-androideabi-clang"):
+            i['arguments'][0] = "/home/ggordon/repo/vendor/persistent/standalone-toolchain/bin/arm-linux-androideabi-clang"
+        if (i['arguments'][0] == "/usr/local/bin/arm-linux-androideabi-clang++"):
+            i['arguments'][0] = "/home/ggordon/repo/vendor/persistent/standalone-toolchain/bin/arm-linux-androideabi-clang++"
+        i['arguments'].insert(1, "--target=armv7a-linux-androideabi28")
+        i['arguments'].insert(1, "--sysroot=/home/ggordon/repo/vendor/persistent/standalone-toolchain/sysroot")
+        i['arguments'].insert(2, "-I")
+        i['arguments'].insert(3, "/home/ggordon/repo/vendor/persistent/standalone-toolchain/sysroot/usr/include")
 
 f.close()
 
